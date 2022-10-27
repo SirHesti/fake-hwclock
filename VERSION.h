d052e8cec3eab608f986d39648e85a30e4fbab81
Sir Hesti <51073539+SirHesti@users.noreply.github.com>
2022-10-27 12:56:56 +0200

    Add files via upload

---
+++ VERSION.h
@@ -0,0 +1,25 @@
+// This is a generated File. Don't modify if you not understand what your are do
+
+#ifndef I_VERSION_H
+#define I_VERSION_H
+
+//Standard Version Type Software Status
+#define I_MAJOR 1
+#define I_MINOR 1
+#define I_BUILD 162
+#define I_BETA "Final"
+
+//Miscellaneous Version Types
+#define I_BuildCount 162
+#define I_BDate "2022-10-26 15:48:57"
+#define I_FullVersion "1.0.162 Beta"
+
+//Extra Infos these values are to keep track of your versioning state
+#define I_SRCDIR "fake-hwclock"
+#define I_PRG "fake-hwclock"
+#define I_FAKE_HWCLOCK_VERSION "1.0.162"
+#define I_FAKE_HWCLOCK_VERSION_NUMBER 100162
+
+#endif // I_VERSION_H
+
+// EOF
