38ccbae5b064f0f9ed8edd0951f1f58fb3fafd54
Sir Hesti <hstools@t-online.de>
2024-04-20 10:21:18 +0200

    Sa 20. Apr 10:21:17 CEST 2024

--- VERSION.h
+++ VERSION.h
@@ -5,22 +5,22 @@
 
 //Standard Version Type Software Status
 #define I_MAJOR 1
-#define I_MINOR 10
-#define I_BUILD 3
-#define I_BETA "Beta"
+#define I_MINOR 11
+#define I_BUILD 0
+#define I_BETA "Finale"
 
 //Miscellaneous Version Types
-#define I_BuildCount 221
-#define I_BDate "2023-12-10 14:39:43"
-#define I_FullVersion "1.10.3 Beta"
-#define I_Version 1.10.3
-#define I_Version_Number 110003
+#define I_BuildCount 222
+#define I_BDate "2024-04-20 10:20:23"
+#define I_FullVersion "1.11.0 Finale"
+#define I_Version 1.11.0
+#define I_Version_Number 111000
 
 //Extra Infos these values are to keep track of your versioning state
 #define I_SRCDIR "fake-hwclock"
 #define I_PRG "fake-hwclock"
-#define I_FAKE_HWCLOCK_VERSION "1.10.3"
-#define I_FAKE_HWCLOCK_VERSION_NUMBER 110003
+#define I_FAKE_HWCLOCK_VERSION "1.11.0"
+#define I_FAKE_HWCLOCK_VERSION_NUMBER 111000
 
 #endif // I_VERSION_H
 
